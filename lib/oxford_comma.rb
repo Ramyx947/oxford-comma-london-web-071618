7810b00a10a81f9a7ca53fe86ab0843cae5e0129
Ramona M <r1mari@yahoo.com>
2018-07-05 16:15:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,32 +1,13 @@
-# def oxford_comma(array)
-# if array.length(1) 
-# puts one_element_array.join(",")
-# elsif two_element_array.length(2)
-# puts two_element_array.join(" and ")
-# elsif three_element_array.length(3)
-# puts three_element_array.join(" , ")
-# else
-#   puts array.join(" , ")
-# end
-# end
-
-# def oxford_comma(array)
-# if one_element_array=["kiwi"]
-# array.join(",")
-# elsif two_element_array=["kiwi", "durian"]
-# two_element_array.join(" and ")
-# elsif three_element_array=["kiwi", "durian", "starfruit"]
-# three_element_array.join(" , ")
-# else
-  
-# end
-# end
 def oxford_comma(array)
-return array[0] if array.length == 1
-  return array[0..-2].join(', ') + " and " + array[-1] if array.length > 1
-end    
-
-
+    if array.count <= 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    last = array.pop
+    array.join(", ") + ", and " << last
+  end
+end
 
 
 

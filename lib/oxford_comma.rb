5f30beb59246268b744290d425039b3fae98711f
Ramona M <r1mari@yahoo.com>
2018-07-05 16:12:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,23 @@
+# def oxford_comma(array)
+# if array.length(1) 
+# puts one_element_array.join(",")
+# elsif two_element_array.length(2)
+# puts two_element_array.join(" and ")
+# elsif three_element_array.length(3)
+# puts three_element_array.join(" , ")
+# else
+#   puts array.join(" , ")
+# end
+# end
+
 def oxford_comma(array)
-array=["fiddleheads","okra","kohlrabi"]
+if one_element_array=["kiwi"]
 array.join(",")
+elsif two_element_array=["kiwi", "durian"]
+two_element_array.join(" and ")
+elsif three_element_array=["kiwi", "durian", "starfruit"]
+three_element_array.join(" , ")
+else
+  
+end
 end
